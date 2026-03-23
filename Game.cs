6df3cc5830540e843349aae31ba9b0149b588995
Jsoosainathan <jsoosainathan@myseneca.ca>
2026-03-23 12:28:23 -0400

    Add Pacman class

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
 ﻿// Include the namespaces (code libraries) you need below.
+using Assignment_4;
 using System;
 using System.Numerics;
 
@@ -12,12 +13,21 @@ namespace MohawkGame2D
     {
         // Place your variables here:
 
-
+        int tilewidth = 16;
+        int tileheight = 16;
+        int rows = 36;
+        int columns = 28;
+        int screenwidth = 448;
+        int screenheight = 512;
+        Pacman pacman = new Pacman();
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            Window.SetTitle("Pacman");
+            Window.SetSize(screenwidth, screenheight);
+
 
         }
 
@@ -26,8 +36,31 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            render();
+            pacman.update();
+
+
 
         }
+
+
+        public void render()
+        {
+
+            pacman.render();
+
+
+
+        }
+
+
+
+
+
+
+
+
     }
+
 
 }
---
+++ Pacman.cs
@@ -0,0 +1,60 @@
+﻿using MohawkGame2D;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_4
+{
+    public class Pacman
+    {
+        public enum Direction
+        {
+            NONE,
+            UP,
+            DOWN,
+            LEFT,
+            RIGHT
+
+        }
+
+        public Pacman()
+        {
+
+            direction = Direction.NONE;
+            x = 10;
+            y = 10;
+            speed = 2;
+        }
+
+        public int x { get; set; }
+        public int y { get; set; }
+        public int lives { get; set;  }
+        public int score { get; set; }
+        public int speed { get; set; }
+        public Direction direction { get; set; }
+        public int radius = 10;
+
+
+        public void update()
+        {
+
+
+
+
+
+
+
+        }
+
+        public void render()
+        {
+
+            Draw.FillColor = Color.Yellow;
+            Draw.Circle(x, y, radius);
+
+        }
+
+        public void handle
+
+    }
+}
